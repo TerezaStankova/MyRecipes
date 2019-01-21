49d7e38027381d7a69c6a61ce18beae0e645d15f
tereza <tstankova@deloittece.com>
2019-01-21 15:49:38 +0100

    Add project files.

---
+++ MyRecipes/MyRecipes/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using MyRecipes.Models;
+
+namespace MyRecipes.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
