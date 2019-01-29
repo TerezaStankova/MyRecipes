d363d1b18e89e42f7d4b5575da277ba6b59ce975
tereza <tstankova@deloittece.com>
2019-01-29 13:51:05 +0100

    style: let the rows with recipes have different lenght

--- MyRecipes/MyRecipes/ViewModels/ItemsViewModel.cs
+++ MyRecipes/MyRecipes/ViewModels/ItemsViewModel.cs
@@ -17,7 +17,7 @@ namespace MyRecipes.ViewModels
 
         public ItemsViewModel()
         {
-            Title = "Browse";
+            Title = "My Recipes";
             Items = new ObservableCollection<Item>();
             LoadItemsCommand = new Command(async () => await ExecuteLoadItemsCommand());
 
